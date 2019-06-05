b218961fd3ead87ad7474694ab2b66599b9adb29
Dmitry Fiodorov <d.fiodorov@vironit.co.uk>
2019-06-05 16:17:44 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import utils
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+
+x_train_normalized = x_train / 255
+
+y_train = utils.to_categorical(y_train, 10)
+
+classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки'] 
+
+model = Sequential()
+
+model.add(Dense(800, input_dim=784, activation="relu"))
+model.add(Dense(10, activation="softmax"))
+
+model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+
+print(model.summary())
+
+model.fit(x_train_normalized, y_train, batch_size=200, epochs=10, verbose=1)
+
+predictions = model.predict(x_train)
+
+print(predictions[0])
+
+print(np.argmax(predictions[0]))
+
+print(y_train[0])
+
